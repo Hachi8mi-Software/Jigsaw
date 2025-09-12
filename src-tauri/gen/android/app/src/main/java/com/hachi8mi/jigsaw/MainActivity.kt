b993dca2df6f99542c336263c23ac94a70f0a3c3
Dustin Jiang <dustinjiang@outlook.com>
2025-09-12 15:49:22 +0800

    🐛 修改Activity避免状态栏冲突

--- src-tauri/gen/android/app/src/main/java/com/hachi8mi/jigsaw/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/hachi8mi/jigsaw/MainActivity.kt
@@ -1,11 +1,22 @@
+
 package com.hachi8mi.jigsaw
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     enableEdgeToEdge()
     super.onCreate(savedInstanceState)
+
+    val content: View = findViewById(android.R.id.content)
+    ViewCompat.setOnApplyWindowInsetsListener(content) { v, insets ->
+      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+      insets
+    }
   }
 }
